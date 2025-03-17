6abb90df098ac1f59d40c9c06197532ee5ef013e
JonPer00 <pj1959@mci4me.at>
2025-03-17 16:44:03 +0100

    Update leistungstest.py

--- leistungstest.py
+++ leistungstest.py
@@ -1,4 +1,39 @@
-#User Case 1.03
-##Dieser Case beschreibt einen Alarm bei einer zu hohen Herfrequenz des Probanten. 
-#Dieser dient dazu, einen größeren bleibenden Schaden zu vermeiden und ist essenziell wichtig für den Zeitlichen Verlauf 
-#des Herzschlages. Zusätzlich dient er als Zeichen, wie seht der Proband angestrengt ist. 
+from datetime import date
+from typing import Dict, List, Any
+
+def create_experiments(first_experiment_id: int, num_experiments: int = 10) -> List[Dict[str, Any]]:
+    today = date.today()
+    experiments: List[Dict[str, Any]] = []
+    even_ids: int = 0
+
+    try:
+        first_experiment_id = int(first_experiment_id)
+    except ValueError:
+        print("Bitte nur Zahlen verwenden")
+        return []
+
+    for i in range(10):
+        experiment_id = first_experiment_id + i
+        if experiment_id % 2 == 0:
+          even_ids +=1
+
+        experiment = {
+            "id": experiment_id,
+            "experimenter": "Johnn",
+            "creation_date": today,
+            "test_name": f"Leistungstest {i+1}"
+        }
+        experiments.append(experiment)
+    print(f"Es wurden {even_ids} gerade ID´s gefunden")
+    return experiments
+
+def display_experiments(experiments: List[Dict[str, Any]], num_to_display: int = 5) -> None:
+    """Gibt die ersten fünf Experimente aus."""
+    for i in range(min(num_to_display, len(experiments))):
+        print(experiments[i])
+first_experiment_id = 187
+
+experiments_list = create_experiments(first_experiment_id)
+
+if experiments_list:
+    display_experiments(experiments_list)
